b20604f20d6d662c1280b160bde587ac86adf07a
jpm <jpmckenna90@gmail.com>
2020-01-06 22:51:20 -0500

    Finished implementing localStorage. City history now populates on refresh, still minor tweaks to be made. Outlined in todo comments.

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-// !TODO what should go into local storage? should it be key: city, property: queryURL? - maybe, this seems to work
-// !TODO make the city list items into buttons, when clicked, pass relevant queryURL into function
+// !todo possibly better handling of the input? verify it, provide an error if nothing can be produced. 
+// !todo add fahrenheit, percentage, etc. you idiot 
+// !todo general cleanup, add comments 
 
 var apiKey = "226c7f48de6d96ec8e61f348613686a1";
 var city = "";
 var now = moment().format("HH:mm");
+var localStorageIndex = 0;
+var cityArray = [];
+var cityObj = {};
 
 $(document).ready(function() {
-  
-  $("#searchbtn").on("click", function() {
-    ajaxCall();
-    $("h6").empty();
-  });
 
-  $("#citylist").on("click", function(){
-    city = event.target.textContent.trim();
-    console.log(city);
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + apiKey;
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) { 
-      clearContent();
-      currentConditions(response);
-      fiveDay(response)
-      console.log(response);
-    });
+  createHistory();
+
+  $("#clearhistory").on("click", function(){
+    clearHistory();
+    $("#citylist").empty();
   })
 
-  // This function clears content to make way for incoming data 
-  function clearContent(){
-    $(".clearme").empty();
-  }
+  $("#searchbtn").on("click", function() {
+    ajaxCall();
+  });
 
-  //This is for current weather - need to implement forecasts
-  function ajaxCall() {
-    city = $("#cityfield").val();
-    // how can I make these clickable? and then call ajax call but not update the list of cities (actually that part should be easy, can
-    // maybe even write a conditional within this function that says if citylist !contain city. might have to bring in an array and ignore case to do that)
-    $("#citylist").prepend("<li class='citylist'><a id='cityitem'>" + city);
-    //prettier-ignore
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + apiKey;
-    window.localStorage.setItem(city, queryURL);
+  $("#citylist").on("click", function() {
+    city = event.target.textContent.trim();
+    console.log(city);
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial" +
+      "&APPID=" +
+      apiKey;
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       clearContent();
       currentConditions(response);
-      fiveDay(response)
+      fiveDay(response);
+      console.log(response);
+    });
+  });
+
+  // This function clears content to make way for incoming data
+  function clearContent() {
+    $(".clearme").empty();
+  }
+
+  //This is for current weather - need to implement forecasts
+  function ajaxCall() {
+    city = $("#cityfield").val();
+    $("#citylist").prepend("<li class='citylist'><a id='cityitem'>" + city);
+    //prettier-ignore
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + apiKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      cityObj["name"] = city;
+      cityObj["ID"] = response.id;
+      window.localStorage.setItem(cityObj["name"], cityObj["ID"]);
+      clearContent();
+      currentConditions(response);
+      fiveDay(response);
       console.log(response);
     });
   }
 
- 
-
   // This function updates card with current city info
   function currentConditions(response) {
-    var currentIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+    var currentIcon =
+      "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
     $("#maincard").text(response.name + ", " + now);
     $("#maincard").append("<img src='" + currentIcon + "'>");
     $("#cardtemp").append("<h5 class='clearme'>" + response.main.temp);
@@ -80,55 +95,55 @@ $(document).ready(function() {
   }
 
   // Function to generate five day forecast
-  function fiveDay(response){
+  function fiveDay(response) {
     var cityID = response.id;
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&units=imperial" + "&appid=" + apiKey,
+      url:
+        "https://api.openweathermap.org/data/2.5/forecast?id=" +
+        cityID +
+        "&units=imperial" +
+        "&appid=" +
+        apiKey,
       method: "GET"
     }).then(function(fiveday) {
 
-      // var iconID = "";
-
-      for(var i = 0; i < 5; i++){
-      $("#day" + [i]).prepend("<h3>" + moment().format("MMMM") + " " + (moment().date() + i));
-      $("#day" + [i]).append("<img src='" + "http://openweathermap.org/img/w/" + fiveday.list[i * 8].weather[0].icon + ".png" + "'>");
-      $("#day" + [i]).append("<h6>" + "Temp: " + fiveday.list[i * 8].main.temp + " &#8457;");
-      $("#day" + [i]).append("<h6>" + "Humidity: " + fiveday.list[i * 8].main.humidity + "%");
-    }
-
-    
+      for (var i = 0; i < 5; i++) {
+        $("#day" + [i]).prepend(
+          "<h3>" + moment().format("MMMM") + " " + (moment().date() + i)
+        );
+        $("#day" + [i]).append(
+          "<img src='" +
+            "http://openweathermap.org/img/w/" +
+            fiveday.list[i * 8].weather[0].icon +
+            ".png" +
+            "'>"
+        );
+        $("#day" + [i]).append(
+          "<h6>" + "Temp: " + fiveday.list[i * 8].main.temp + " &#8457;"
+        );
+        $("#day" + [i]).append(
+          "<h6>" + "Humidity: " + fiveday.list[i * 8].main.humidity + "%"
+        );
+      }
     });
   }
 
-  for(var i = 0; i < localStorage.length; i++){
-    var current
-    // if the city in localstorage is not in the citylist, add it
-    if(!$("#cityitem").textContent === (JSON.stringify(localStorage.key(i)))){
-      $("#citylist").prepend("<li class='citylist'><a id='cityitem'>" + JSON.stringify(localStorage.key(i)));
+  function createHistory() {
+    for (var i = 0; i < localStorage.length; i++) {
+      // Variable to hold item of current place in local storage
+      var currentItem = JSON.stringify(localStorage.key(i));
+      // Check to see if the current item is already in the array of cities
+      if (!cityArray.includes(currentItem)) {
+      // If not, add it. Just add the key - this will be used to retrieve the ID for the AJAX call (methinks)
+        cityArray.push(localStorage.key(i));
+      }
+      $("#citylist").prepend("<li class='citylist'><a id='cityitem'>" + localStorage.key(i));
     }
+    console.log(cityArray);
   }
-}
-);
+});
 
-// ! CODE BELOW is localStorage calling to add to dom from javascript quiz - possibly use for reference? 
+  function clearHistory(){
+    localStorage.clear();
+  }
 
-// $("#initialSubmission").show();
-// $("#initialSubmission")
-//   .unbind("click")
-//   .on("click", function() {
-//     var recordString = $("#initials").val() + "," + userScore;
-//     $("#initials").val("");
-//     recordArray.push(recordString);
-//     var tableValues = recordString.split(",");
-//     $("#scoreTable").append(
-//       "<tr><td>" +
-//         tableValues[0] +
-//         "</td><td>" +
-//         tableValues[1] +
-//         "</td></tr>"
-//     );
-//     $(this).off();
-//     window.localStorage.setItem(localStorageIndex, recordString);
-//     localStorageIndex++;
-    
-//   });
